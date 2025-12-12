d09850c5d7c3bfe1800326e92661ba69e668ec27
Kanchan <kanchandhamala1@gmail.com>
2025-12-12 15:57:35 +0545

    register controller bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, username, password } = req.body;
-  console.log(req.body);
+  //console.log(req.body);
 
   // if (fullName === ""){
   //     throw new ApiError(400,"Full name is required");
@@ -25,17 +25,24 @@ const registerUser = asyncHandler(async (req, res) => {
   if (existedUser) {
     throw new ApiError(409, "User with given username or email already exists");
   }
-  console.log(req.files);
+  //console.log(req.files);
   
 
-  const avatarLoalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  
 
-  if (!avatarLoalPath) {
+  let coverImageLocalPath;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length >0 ){
+    coverImageLocalPath= req.files.coverImage[0].path;
+  }
+
+
+  if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
   }
 
-  const avatar = await uploadOnCloudinary(avatarLoalPath);
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
   if (!avatar) {
