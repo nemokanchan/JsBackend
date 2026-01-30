a4f4c2f59e2631ec8498d5ab1c353de74aecabce
Kanchan <kanchandhamala1@gmail.com>
2026-01-30 23:55:35 +0545

    RefreshToken

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { loginUser, registerUser , logOutUser } from '../controllers/user.controller.js';
+import { loginUser, registerUser , logOutUser , refreshAccessToken } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT }   from '../middlewares/auth.middleware.js';
 
@@ -24,5 +24,6 @@ router.route("/login").post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJWT,logOutUser)
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
